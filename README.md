ba3da6e51ae5c2bf00ea4c9b3f7d922d469c4234
Kaitlyn Mooney <kaitlynmooney@kaitlyns-mbp.myfiosgateway.com>
2024-06-12 17:31:47 -0400

    added screenshot and updated readme

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# hw4-personal-blog
+# Personal Blog
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- What was your motivation?
+- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+- What problem does it solve?
+- What did you learn?
+
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+
+    ![Deployed application screenshot](assets/images/hw4-personal-blog-screenshot.png)
+  
+Deployed Application Link: 
--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,37 +1,43 @@
 const themeSwitcher = document.querySelector('#theme-switcher');
 const container = document.querySelector('.container');
-let mode = 'dark';
+let mode = 'light';
 const anchorEl = document.querySelector('#blog-anchor');
 
 themeSwitcher.addEventListener('click', function () {
 
-  if (mode === 'dark') {
-    mode = 'light';
-    container.setAttribute('class', 'light');
+  if (mode === 'light') {
+    mode = 'dark';
+    container.setAttribute('class', 'dark');
   }
 
   else {
-    mode = 'dark';
-    container.setAttribute('class', 'dark');
+    mode = 'light';
+    container.setAttribute('class', 'light');
   }
 });
 function blogFormStorage() {
 
   const savedBlogs = JSON.parse(localStorage.getItem('blogs'));
   if (!savedBlogs) {
-      return []
-      } else {
-      return savedBlogs;
-      };
+    return []
+  } else {
+    return savedBlogs;
   };
+};
 const retrievedBlogs = blogFormStorage();
-for (let i = 0; i < retrievedBlogs.length; i++ ) {
+for (let i = 0; i < retrievedBlogs.length; i++) {
   const cardBodyEl = document.createElement('div');
+  cardBodyEl.setAttribute('class', 'card');
   const titleEl = document.createElement('h1');
   const contentEl = document.createElement('p');
-  const authorEl = document.createElement('p');
+  const usernameEl = document.createElement('p');
   retrievedBlogs[i]
   titleEl.textContent = retrievedBlogs[i].title;
-anchorEl.appendChild(cardBodyEl);
-cardBodyEl.appendChild(titleEl);
+  contentEl.textContent = retrievedBlogs[i].content;
+  usernameEl.textContent = retrievedBlogs[i].username;
+  anchorEl.appendChild(cardBodyEl);
+  cardBodyEl.appendChild(titleEl);
+  cardBodyEl.appendChild(contentEl);
+  cardBodyEl.appendChild(usernameEl);
+  console.log(blogFormStorage);
 }
